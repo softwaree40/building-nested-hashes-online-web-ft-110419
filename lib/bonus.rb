b2ede8cd7b3df2ef5ada1ca14bfe145a0ffe99d5
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-07 21:41:36 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,8 +22,8 @@ def bonus
   }
 
   #code your solution here:
-   #binding.pry
-  epic_tragedy[:montague][:hero][status:]= "dead"
+   binding.pry
+  epic_tragedy[:montague][:hero] = "dead"
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
